695af60ebbd94a3138a8c47fb391d0e7528d0226
mrm <mrlager@protonmail.com>
2022-03-11 16:41:04 +0000

    Syntax fix

--- backup.sh
+++ backup.sh
@@ -5,26 +5,26 @@
 #+  Packages needed: rsync and openssh
 #   Architecture: Tested and working on x86-64 and aarch64
 
-eval $(ssh-agent)
-ssh-add /home/mrm/.ssh/my-servers
+eval $(ssh-agent)                             # 
+ssh-add /home/LOCAL_USER/.ssh/MY_SSH_KEY
 
 
 # Use while loop and ping the backup server.
-IP="10.0.0.10"    #CHANGE THIS
+IP="REMOTE_IP_HERE"                           #CHANGE THIS
 
 # Maximum number to try
 ((count = 10))								
 
 while [[ $count -ne 0 ]] ; do
-    ping -c 1 $IP 2>&1 >/dev/null			# Try once.
+    ping -c 1 $IP 2>&1 >/dev/null			        # Try once.
     rt=$?
     if [[ $rt -eq 0 ]] ; then
-        break		                      	# If okay, break the loop with "rt"
+        break		                      	      # If okay, break the loop with "rt"
     fi
-    ((count = count - 1))                  	# Count 1 so we don't go on forever
+    ((count = count - 1))                  	  # Count 1 so we don't go on forever
 done
 
-if [[ $rt -eq 0 ]] ; then                 	# Final check
+if [[ $rt -eq 0 ]] ; then                 	  # Final check
     echo "Connection established.";
 else
     echo "Connection failed."; 
@@ -34,8 +34,8 @@ fi
 # Config
 DIRECTORY='backup'      
 TIMESTAMP=$(date "+%Y-%m-%d-%H:%M:%S")
-REMOTEUSER=mumriken     #EXAMPLE
-LOCALUSER=mrm           #EXAMPLE
+REMOTEUSER=USERNAME                           #EXAMPLE
+LOCALUSER=LOCAL_USERNAME                      #EXAMPLE
 SSH="ssh -p 1027 -i /home/mrm/.ssh/backup-servers"
 
 
@@ -47,16 +47,16 @@ cp -aR /etc "/home/$LOCALUSER/$DIRECTORY/$TIMESTAMP" 2>/dev/null >/dev/null
 
 #set -xv
 #Check entries / are there any older backups?
-BACKUP_ENTRIES=$(ssh -p 1027 -i /home/mrm/.ssh/backup-servers $REMOTEUSER@$IP "ls /home/$REMOTEUSER/$DIRECTORY | wc -l")
+BACKUP_ENTRIES=$(ssh -p 22 -i /home/$LOCALUSER/.ssh/YOUR_SSH_KEY $REMOTEUSER@$IP "ls /home/$REMOTEUSER/$DIRECTORY | wc -l")
 if [ $BACKUP_ENTRIES -gt 0 ]
 then
     echo "Creating snapshot..."
-    LATEST=$(ssh -p 1027 -i /home/mrm/.ssh/backup-servers $REMOTEUSER@$IP "ls /home/$REMOTEUSER/$DIRECTORY | tail -1") # Will check if there is any existing backup
+    LATEST=$(ssh -p 22 -i /home/$LOCALUSER/.ssh/YOUR_SSH_KEY $REMOTEUSER@$IP "ls /home/$REMOTEUSER/$DIRECTORY | tail -1")          # Will check if there is any existing backup
     $SSH $REMOTEUSER@$IP "cp -al /home/$REMOTEUSER/$DIRECTORY/$LATEST /home/$REMOTEUSER/$DIRECTORY/$TIMESTAMP 2>/dev/null >/dev/null"
 fi
 
 # Transfer the local backup to main server. rsync will only transfer changed or added files in /etc.
-rsync -avEPhze "ssh -p 1027 -i /home/mrm/.ssh/backup-servers" "/home/$LOCALUSER/$DIRECTORY/$TIMESTAMP" "$REMOTEUSER@$IP:/home/$REMOTEUSER/$DIRECTORY/" # Finally back
+rsync -avEPhze "ssh -p 22 -i /home/$LOCALUSER/.ssh/MY_SSH_KEY" "/home/$LOCALUSER/$DIRECTORY/$TIMESTAMP" "$REMOTEUSER@$IP:/home/$REMOTEUSER/$DIRECTORY/" # Finally back
 #-avEphz
 
 # END
--- restore.sh
+++ restore.sh
@@ -6,12 +6,12 @@
 #	Architecture: Tested and working on aarch64 and x86-64
 
 # Config
-IP="10.0.0.10"           #EXAMPLE - Remote host IP
-DIRECTORY='backup'      #EXAMPLE - Backup directory
-REST_DIR='restored'     #Example - Restore backup directory
+IP="REMOTE_IP_HERE"                     #EXAMPLE - Remote host IP
+DIRECTORY='backup'                      #EXAMPLE - Backup directory
+REST_DIR='restored'                     #Example - Restore backup directory
 TIMESTAMP=$(date "+%Y-%m-%d-%H:%M:%S")
-REMOTEUSER=mumriken     #EXAMPLE - Remote user
-LOCALUSER=mrm           #EXAMPLE - Local user
+REMOTEUSER=REMOTE_USER                  #EXAMPLE - Remote user
+LOCALUSER=LOCAL_USER                    #EXAMPLE - Local user
 
 # Maximum number to try (Ping the remote server)
 ((count = 10))
@@ -20,12 +20,12 @@ while [[ $count -ne 0 ]] ; do
     ping -c 1 $IP 2>/dev/null 1>/dev/null	# Try once.
     rt=$?
     if [[ $rt -eq 0 ]] ; then
-        break		                      	# If okay, exit the loop with "rt"
+        break		                      	  # If okay, exit the loop with "rt"
     fi
-    ((count = count - 1))                  	# Count 1 so we don't go on forever
+    ((count = count - 1))                 # Count 1 so we don't go on forever
 done
 
-if [[ $rt -eq 0 ]] ; then                  	# Final check
+if [[ $rt -eq 0 ]] ; then                 # Final check
     echo "Connection established.";
 
 else
@@ -34,9 +34,9 @@ else
 fi  
 	
 # Check if there are any available backups on the remove server
-AVAILABLE_BACKUPS=$(ssh -p 1027 -i /home/mrm/.ssh/my-servers $REMOTEUSER@$IP "ls $DIRECTORY")
+AVAILABLE_BACKUPS=$(ssh -p 22 -i /home/$LOCALUSER/.ssh/MY_SSH_KEY $REMOTEUSER@$IP "ls $DIRECTORY")
 select BACKUP in $AVAILABLE_BACKUPS
 do
-    rsync -avEPhze "ssh -p 1027 -i /home/mrm/.ssh/backup-servers" $REMOTEUSER@$IP:/home/$REMOTEUSER/$DIRECTORY/$BACKUP /home/$LOCALUSER/$DIRECTORY/restored/
+    rsync -avEPhze "ssh -p 22 -i /home/$LOCALUSER/.ssh/MY_SSH_KEY" $REMOTEUSER@$IP:/home/$REMOTEUSER/$DIRECTORY/$BACKUP /home/$LOCALUSER/$DIRECTORY/restored/
     break
 done
